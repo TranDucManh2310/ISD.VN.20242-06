716b864273c5cfbd7d7a92e68aebb262824c75ba
quanglazy <quangcv301@gmail.com>
2025-06-03 00:32:18 +0700

    update route

--- src_code/backend/routes/category.js
+++ src_code/backend/routes/category.js
@@ -1,1 +1,15 @@
-#test
+const express = require("express");
+const router = express.Router();
+
+const {
+  newCategory,
+  getCategory,
+  deleteCategory,
+} = require("../controllers/categoryController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+
+router.route("/admin/genres/addgenre").post(newCategory);
+router.route("/genres").get(getCategory);
+router.route("/movies/:genreID").delete(deleteCategory);
+
+module.exports = router;
