5b9c48d38f0674e9f99b3548e3e31f5c3117dc09
nguyen-huy-hoang-1708 <nguyenhuyhoangtuho@gmail.com>
2025-06-03 21:28:24 +0700

    up

--- src_code/backend/routes/payment.js
+++ src_code/backend/routes/payment.js
@@ -1,1 +1,30 @@
-#test
+const express = require('express')
+const router = express.Router();
+
+const {
+   receiveHookPayment,createPaymentLink
+} = require('../controllers/paymentController')
+
+const { isAuthenticatedUser } = require('../middlewares/auth')
+
+// router.route('/payment/process').post(isAuthenticatedUser, processPayment);
+// router.route('/stripeapi').get(isAuthenticatedUser, sendStripApi);
+router.route("/payment/create-payment-link").post(createPaymentLink)
+router.route("/payment/receive-hook").post(receiveHookPayment)
+
+
+/*const { generateBankQR, generateMoMoQR } = require('../utils/qrCode');
+
+router.get('/bank', (req, res) => {
+  const { bankCode, accountNumber, amount, description } = req.query;
+  const qrUrl = generateBankQR(bankCode, accountNumber, amount, description);
+  res.json({ qrUrl });
+});
+
+router.get('/momo', (req, res) => {
+  const { phoneNumber, amount, description } = req.query;
+  const qrUrl = generateMoMoQR(phoneNumber, amount, description);
+  res.json({ qrUrl });
+});*/
+
+module.exports = router;
