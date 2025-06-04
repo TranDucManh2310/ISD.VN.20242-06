3ae2330544c614665ae5f9d96d1180ba55856aa1
phamminh <providminh24092004@gmail.com>
2025-06-04 22:37:15 +0700

    update frontend

--- src_code/frontend/tailwind.config.js
+++ src_code/frontend/tailwind.config.js
@@ -1,1 +1,12 @@
-#test
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'orange':"#f96822",
+      'yellow':"#ffa500",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
