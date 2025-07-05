1da446c5c46409d8d9ffda9c1824293d7b82d441
TranDucManh2310 <tranducmanh23102004@gmail.com>
2025-07-06 03:01:03 +0700

    update db

--- src_code/backend/config/database.js
+++ src_code/backend/config/database.js
@@ -2,16 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    const con = await mongoose
-    .connect('mongodb://localhost:27018/shopit1', {
+    const con = await mongoose.connect("mongodb://localhost:27017/shopit1", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    })
+    });
     console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
-  
   } catch (error) {
-  console.log(error)    
+    console.log(error);
   }
 };
 
